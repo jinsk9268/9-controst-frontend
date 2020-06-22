ec78aaca9ef929888992d185509a712efd2fe34d
Mingyu-Song <sv002@naver.com>
2020-06-22 19:29:21 +0900

    [add] Front 초기세팅

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component= {Main} />
+          <Route exact path="/login" component= {Login} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
