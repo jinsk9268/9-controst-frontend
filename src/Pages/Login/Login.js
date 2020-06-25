1c1f50b5f3986b5e55c5a058e94672a2ae635b08
김민지 <imjkim49@github.com>
2020-06-25 20:22:32 +0900

    [add] Nav, Login 추가

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 export class Login extends React.Component {
-  
+
 
 render() {
         return (
@@ -14,29 +14,28 @@ render() {
                 <button className="login-close-btn" type="button">닫기</button>
                 <form className="login-form"> 
                 <div className="login-btn-wrap">
-               
+
                 <button type="button" className="login-btn-kakao" >  카카오톡 로그인</button>
                 <button type="button" className="login-btn-facebook" >페이스북 로그인</button>
                 <button type="button" className="login-btn-naver" >네이버 로그인</button>
                 <button type="button" className="login-btn-apple" >애플 로그인</button>
-           
+
 
                 <div className="login-email">
                   <Link to="#" className="login-email-a">이메일로 로그인</Link>
                 </div>
                 </div>
                 <div className="login-info">트로스트 계정이 없으신가요?
-                 
+
                   <Link to="#" className="sign-link">회원가입하기</Link>
                 </div>
                 </form>
-                
+
             </div>
             </div>
         </div>
-  
+
         )
     }
 }
 export default Login;
-
