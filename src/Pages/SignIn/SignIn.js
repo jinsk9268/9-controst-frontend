419fe872477736fda8bd0e9128e39e81cb8fc44c
김민지 <imjkim49@github.com>
2020-06-26 18:46:15 +0900

    [add] signin레이아웃 구성

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,70 +1,106 @@
-
-import React from 'react';
-import './SignIn.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./SignIn.scss";
+import { Link } from "react-router-dom";
 
 class SignIn extends React.Component {
-    constructor (){
-        super();
-        this.state = {
-          id:'',
-          pw:'',
-        }
-      }
-        idChangeEvent = e => {
-            this.setState({id:e.target.value});
-            console.log('id >>> ',this.state.id)
-        }
-    
-        pwChangeEvent = e => {
-            this.setState({pw:e.target.value})
-            console.log('pw >>> ',this.state.pw)
-        }
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      pw: "",
+    };
+  }
+  idChangeEvent = (e) => {
+    this.setState({ id: e.target.value });
+    console.log("id >>> ", this.state.id);
+  };
 
-        clickHandler = () => {
-            //e.prevent.Default();
-              //POST
-              fetch("http://10.58.6.241:8000/users/signin", {
-                  method:"POST",
-                  body: JSON.stringify({
-                      email: this.state.id,
-                      password: this.state.pw
-                  })//이 )소괄호는 아직 fetch에 소괄호니까 밑에 then 가능
-              })
-              .then(res => res.json())// 제이슨 바디로 온다...
-              .then(res=> console.log(res));//그냥 콘솔에 뿌리고 끝 리턴 안함
-          };
+  pwChangeEvent = (e) => {
+    this.setState({ pw: e.target.value });
+    console.log("pw >>> ", this.state.pw);
+  };
 
-render() {
-        return (
-            <div className="signIn-container"> 
-                <h2 className="signIn-head">이메일 로그인</h2> 
-                <div className="signIn-input-box"> 
-                <label for="userEmailLogin"> 
-                  <em className="user-head">아이디</em> 
-                  <input type="text" onChange={this.idChangeEvent} className="idinput" id="userEmailLogin" maxlength="50" autofocus="" /> 
-                </label> 
-                <div className="signIn-npminfo" id="emailLoginMsg"> </div>
-            </div> 
-            <div className="signIn-input-box"> 
-                <label for="userPwLogin"> 
-                  <em className="user-head">비밀번호</em>
-                  <input type="password" onChange={this.pwChangeEvent} className="pwinput"  minlength="6" maxlength="14" /> 
-                </label> 
-                <div className="signIn-info" id="pwLoginMsg"></div>
-                <div className="signIn-bottom-note"> 
-                  <div id="loginMsg"></div> 
-                 </div> 
-                </div>
-                <button onClick={this.clickHandler} className="main-btn-active" type="button" id="btnLoginEmail" style = {{backgroundColor:this.state.id.includes('@') && this.state.pw.length>5?"#e066ff":"#0094f69f"}}>로그인</button> 
-                 <div className="login-info-txt"> 
-                    <button className="login-link-join" id="goJoinMain2" type="button">회원가입</button>
-                    <button className="login-link-join" id="gofindPw" type="button">비밀번호 찾기</button> 
-                 </div> 
-                 <button id="popupLoginClose" class="join-style-popup-close js-popup-btn-close" type="button">닫기</button>  
-                </div>
-    
-        )
-    }
+  clickHandler = () => {
+    //e.prevent.Default();
+    //POST
+    fetch("http://10.58.6.241:8000/users/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email: this.state.id,
+        password: this.state.pw,
+      }), //이 )소괄호는 아직 fetch에 소괄호니까 밑에 then 가능
+    })
+      .then((res) => res.json()) // 제이슨 바디로 온다...
+      .then((res) => console.log(res)); //그냥 콘솔에 뿌리고 끝 리턴 안함
+  };
+
+  render() {
+    return (
+      <div className="signIn-container">
+        <h2 className="signIn-head">이메일 로그인</h2>
+        <div className="signIn-input-box">
+          <label for="userEmailLogin">
+            <em className="user-head">아이디</em>
+            <input
+              type="text"
+              onChange={this.idChangeEvent}
+              className="idinput"
+              id="userEmailLogin"
+              maxlength="50"
+              autofocus=""
+            />
+          </label>
+          <div className="signIn-npminfo" id="emailLoginMsg">
+            {" "}
+          </div>
+        </div>
+        <div className="signIn-input-box">
+          <label for="userPwLogin">
+            <em className="user-head">비밀번호</em>
+            <input
+              type="password"
+              onChange={this.pwChangeEvent}
+              className="pwinput"
+              minlength="6"
+              maxlength="14"
+            />
+          </label>
+          <div className="signIn-info" id="pwLoginMsg"></div>
+          <div className="signIn-bottom-note">
+            <div id="loginMsg"></div>
+          </div>
+        </div>
+        <button
+          onClick={this.clickHandler}
+          className="main-btn-active"
+          type="button"
+          id="btnLoginEmail"
+          style={{
+            backgroundColor:
+              this.state.id.includes("@") && this.state.pw.length > 5
+                ? "#e066ff"
+                : "#0094f69f",
+          }}
+        >
+          로그인
+        </button>
+        <div className="login-info-txt">
+          <button className="login-link-join" id="goJoinMain2" type="button">
+            회원가입
+          </button>
+          <button className="login-link-join" id="gofindPw" type="button">
+            비밀번호 찾기
+          </button>
+        </div>
+        <button
+          id="popupLoginClose"
+          class="join-style-popup-close js-popup-btn-close"
+          type="button"
+        >
+          닫기
+        </button>
+      </div>
+    );
+  }
 }
 export default SignIn;
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import Main from './Pages/Main/Main';
-import Login from './Pages/Login/Login';
-import SignIn from './Pages/SignIn/SignIn';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
+import SignIn from "./Pages/SignIn/SignIn";
 
 class Routes extends React.Component {
   render() {
