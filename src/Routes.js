174da0370dbda8e7aa10607f7fb8ffdb3c565a17
김민지 <imjkim49@github.com>
2020-06-26 17:22:59 +0900

    Signin 페이지 생성

--- src/Routes.js
+++ src/Routes.js
@@ -6,14 +6,18 @@ import {
 } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
+import SignIn from './Pages/SignIn/SignIn';
+
+
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component= {Main} />
-          <Route exact path="/login" component= {Login} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signin" component={SignIn} />
         </Switch>
       </Router>
     );
