29cfa9fbfbfd6b69ba2a43f98ccc9fec2084929e
Mingyu-Song <sv002@naver.com>
2020-07-01 02:09:52 +0900

    [add]상담센터 리스트 클릭시 마커 selected 이미지구현

--- src/Pages/Offline/Map.js
+++ src/Pages/Offline/Map.js
@@ -7,6 +7,8 @@ class Map extends Component {
         this.state = {
             location : "",
             clickIndex : "",
+            positions: [],
+            marker: {},
         }
 
         
@@ -122,6 +124,8 @@ class Map extends Component {
                     }
                 ];
 
+               
+
                 // 마커 이미지의 이미지 주소입니다
                     
                     
@@ -142,6 +146,7 @@ class Map extends Component {
                     });
           
                     kakao.maps.event.addListener(marker[i], 'click', function() {
+                        console.log("heheheheh", kakao.maps);
                         for(let j in positions){            //이미지 unSelect로 초기화, z-index 초기화
                             var disableImage = new kakao.maps.MarkerImage(
                                 positions[j]["unSelectedImg"],
@@ -155,7 +160,11 @@ class Map extends Component {
                         marker[i].setImage(markerImage);
                         marker[i].setZIndex(3);         //z-index 설정해준다.
                     });
-                }
+                };
+                this.setState({
+                    marker,
+                    positions
+                });
 
                 // var clickImage = this.props.clickIndex && marker[this.props.clickIndex].setImage(new kakao.maps.MArkerImage(
                 //     positions[this.props.clickIndex]["SelectedImg"],
@@ -165,123 +174,48 @@ class Map extends Component {
             this.setState({
                 location : this.props.location,
             })
-            console.log(this.props);
 
         }
     }
-   
+
+    // markerHandler = (i) => {
+    //     for(let j in positions){            //이미지 unSelect로 초기화, z-index 초기화
+    //         var disableImage = 
+    //             positions[j]["unSelectedImg"];
+    //         marker[j].setImage(disableImage);
+    //         marker[j].setZIndex(2);
+    //     }
+    //     var markerImage =
+    //         positions[i]["SelectedImg"];
+    //     marker[i].setImage(markerImage);
+    //     marker[i].setZIndex(3);         //z-index 설정해준다.
+    // }
+
+
     componentDidUpdate(prevProps){
+        const { kakao } = window;
+        console.log('here');
         if(this.props.clickIndex){
             if(prevProps !== this.props){
                 this.setState({
                     clickIndex : this.props.clickIndex,
-                })
-                const script = document.createElement('script');
-                script.async = true;
-                script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=76a449f1dae5315175afe4994035d80d&libraries=LIBRARY";
-                script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=76a449f1dae5315175afe4994035d80d&libraries=services";
-                script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=76a449f1dae5315175afe4994035d80d&autoload=false";
-                document.head.appendChild(script);
-                script.onload = () => {
-                    const { kakao } = window;
-                    kakao.maps.load(() => {
-                        var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
-                        mapOption = { 
-                            center: new kakao.maps.LatLng(37.504481, 127.049007), // 지도의 중심좌표
-                            level: 5 // 지도의 확대 레벨
-                        };
-
-                    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-                    
-
-                    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-                    var zoomControl = new kakao.maps.ZoomControl();
-                    map.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
-                    // 마커를 표시할 위치와 title 객체 배열입니다 
-
-                    let validationIndex = () => { //알파벳 인덱스 함수
-                        let imgIndexArr = [];
-                        let alphabetArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"];
-                        for(let i in this.state.location){
-                            if(this.state.location[i]["vip"] === true){
-                                imgIndexArr.push('trost');
-                            }
-                        }
-                        let alphabetRemovedArr = alphabetArr.splice(imgIndexArr.length, alphabetArr.length-1);
-                        let resultArr = imgIndexArr.concat(alphabetRemovedArr);
-                        return resultArr;
+                }, ()=> {
+                    let marker = this.state.marker;
+                    let positions = this.state.positions;
+                    for(let j in positions){            //이미지 unSelect로 초기화, z-index 초기화
+                        var disableImage = new kakao.maps.MarkerImage(
+                            positions[j]["unSelectedImg"],
+                            new kakao.maps.Size(30, 30));
+                        marker[j].setImage(disableImage);
+                        marker[j].setZIndex(2);
                     }
-                    var positions = [
-                        {
-                            title: this.state.location[0]["center"], 
-                            latlng: new kakao.maps.LatLng(this.state.location[0]["coordinate"].split(",")[0], this.state.location[0]["coordinate"].split(",")[1]),
-                            unSelectedImg : `https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_unselected_mark/${validationIndex()[0]}_unselected.png`,
-                            SelectedImg : `https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_selected_mark/${validationIndex()[0]}_selected.png`
-                        },
-                        {
-                            title: this.state.location[1]["center"],
-                            latlng: new kakao.maps.LatLng(this.state.location[1]["coordinate"].split(",")[0], this.state.location[1]["coordinate"].split(",")[1]),
-                            unSelectedImg :`https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_unselected_mark/${validationIndex()[1]}_unselected.png`,
-                            SelectedImg : `https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_selected_mark/${validationIndex()[1]}_selected.png`
-                        },
-                        {
-                            title: this.state.location[2]["center"],
-                            latlng: new kakao.maps.LatLng(this.state.location[2]["coordinate"].split(",")[0], this.state.location[2]["coordinate"].split(",")[1]),
-                            unSelectedImg :`https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_unselected_mark/${validationIndex()[2]}_unselected.png`,
-                            SelectedImg : `https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_selected_mark/${validationIndex()[2]}_selected.png`
-                        },
-                        {
-                            title: this.state.location[3]["center"],
-                            latlng: new kakao.maps.LatLng(this.state.location[3]["coordinate"].split(",")[0], this.state.location[3]["coordinate"].split(",")[1]),
-                            unSelectedImg :`https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_unselected_mark/${validationIndex()[3]}_unselected.png`,
-                            SelectedImg : `https://d2qrvi4l1nprmf.cloudfront.net/images/service/mobile/offline/is_selected_mark/${validationIndex()[3]}_selected.png`
-                        }
-                    ];
-
-                    // 마커 이미지의 이미지 주소입니다
-                        
-                        
-                    // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(30, 30); 
-                    
-                    // 마커 이미지를 생성합니다    
-                    
-                    // 마커를 생성합니다
-                    let marker = {};
-                    for(let i = 0; i < positions.length; i++){
-                        var markerImage = new kakao.maps.MarkerImage(positions[i]["unSelectedImg"], imageSize); 
-                        marker[i] = new kakao.maps.Marker({
-                            map: map, // 마커를 표시할 지도
-                            position: positions[i].latlng, // 마커를 표시할 위치    
-                            title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                            image : markerImage // 마커 이미지 
-                        });
-            
-                        kakao.maps.event.addListener(marker[i], 'click', function() {
-                            for(let j in positions){            //이미지 unSelect로 초기화, z-index 초기화
-                                var disableImage = new kakao.maps.MarkerImage(
-                                    positions[j]["unSelectedImg"],
-                                    new kakao.maps.Size(30, 30));
-                                marker[j].setImage(disableImage);
-                                marker[j].setZIndex(2);
-                            }
-                            var markerImage = new kakao.maps.MarkerImage(
-                                positions[i]["SelectedImg"],
-                                new kakao.maps.Size(40, 60), new kakao.maps.Point(20, 60));
-                            marker[i].setImage(markerImage);
-                            marker[i].setZIndex(3);         //z-index 설정해준다.
-                        });
-                    }
-
-                        marker[this.state.clickIndex] = new kakao.maps.Marker({
-                            map: map, // 마커를 표시할 지도
-                            position: positions[this.state.clickIndex].latlng, // 마커를 표시할 위치    
-                            title : positions[this.state.clickIndex].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                            image : new kakao.maps.MarkerImage(positions[this.state.clickIndex]["SelectedImg"], new kakao.maps.Size(40, 60))
-                        })
-                    
-                    })
-                }
+                    var markerImage = new kakao.maps.MarkerImage(
+                        positions[this.state.clickIndex]["SelectedImg"],
+                        new kakao.maps.Size(40, 60), new kakao.maps.Point(20, 60));
+                    marker[this.state.clickIndex].setImage(markerImage);
+                    marker[this.state.clickIndex].setZIndex(3);         //z-index 설정해준다.
+                });
+                
             }
         }
     }
